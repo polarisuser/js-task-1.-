58a626080ca7b44fdc3cc32ae7a1108c9f0ba724
Aiperi Myimanbai <aiperi.myimanbaikyzy.pls@gmail.com>
2022-01-13 18:18:07 +0600

    add task5

---
+++ js/task5.js
@@ -0,0 +1,14 @@
+// let a = Numbe(+prompt("введите число a"));
+// let b = Numbe(+prompt("введите число b"));
+
+// let a = 30;
+// let b = 40;
+// console.log(a + b);
+
+// let a = 55;
+// let b = 65;
+// console.log(a * b);
+
+// let a = 100;
+// let b = 15;
+// console.log(a / b);
